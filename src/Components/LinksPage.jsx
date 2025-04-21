4383caf285a93f1c61c27cfd4ed79369fde5fa05
Owen Tarka <isholatarka@gmail.com>
2025-04-21 00:49:32 -0400

    finalized v0.1

--- src/Components/LinksPage.jsx
+++ src/Components/LinksPage.jsx
@@ -16,10 +16,31 @@ function LinksPage()
             <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=mqNHIK07P3g&t=1476s" target="_blank" className=" hover:text-blue-300">The Disturbing Development of a Haunted Game...</a></div>
             <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=-8EasZ_wSBc&t=1334s" target="_blank" className=" hover:text-blue-300">The Digital Horror Rage Game About Loss...</a></div>
             <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=nZ1Oa_uHsLo" target="_blank" className=" hover:text-blue-300">Pinocchio is a Story About Art and God</a></div>
+            
+            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=Rbrd61I9_6Q" target="_blank" className=" hover:text-blue-300">Beyblade Metal Masters: The Guard Dog of Hades: Kerbecs - Ep.90</a></div>
+            <ol className="list-disc list-inside text-gray-500"><li>This episode man... iconic...</li></ol>
+            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=F4AJfasBRxA" target="_blank" className=" hover:text-blue-300">Steven Universe | Stronger Than You - Song | Cartoon Network</a></div>
+            <ol className="list-disc list-inside text-gray-500"><li>cmon now... this is the good stuff! lock in!</li></ol>
+            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=1o8GWhoD53I" target="_blank" className=" hover:text-blue-300">Puss in Boots: The Last Wish (2022) - Puss in Boots vs. Death Scene</a></div>
+            <ol className="list-disc list-inside text-gray-500"><li>fear me if you dare!</li></ol>
+            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=9-XbXBq8cl8" target="_blank" className=" hover:text-blue-300">Sing About Me, I'm Dying Of Thirst</a></div>
+            <ol className="list-disc list-inside text-gray-500"><li>"Am I worth it? Did I put enough work in?"</li></ol>
+            <ol className="list-disc list-inside text-gray-500"><li>"I said when the lights shut off and it's my turn to settle down my main concern promise that you will sing about me promise that you will sing about me"</li></ol>
             <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=TiH2SjjowKw&t=2700s" target="_blank" className=" hover:text-blue-300">Who's Lila? - Story Explained</a></div>
             <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=s2699HTY-g8" target="_blank" className=" hover:text-blue-300">Hey, Squidward...</a></div>
-            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=mPXLP9mYRi4&t=646s" target="_blank" className=" hover:text-blue-300">if i'm you and you're me then...who's that
-</a></div>
+            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=mPXLP9mYRi4&t=646s" target="_blank" className=" hover:text-blue-300">if i'm you and you're me then...who's that</a></div>
+            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=9-XbXBq8cl8" target="_blank" className=" hover:text-blue-300">Mortal Man</a></div>
+            <ol className="list-disc list-inside text-gray-500"><li>my favourite song ever</li></ol>
+            <ol className="list-disc list-inside text-gray-500"><li>"when shit hit the fan is you still a fan? wont you look to the left and right make sure you ask your friends"</li></ol>
+            <ol className="list-disc list-inside text-gray-500"><li>YALL ARE NOT LISTENING!!!!</li></ol>
+            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://www.youtube.com/watch?v=_tWh4cYCTv0&t=595s" target="_blank" className=" hover:text-blue-300">how i made my website</a></div>
+            <ol className="list-disc list-inside text-gray-500"><li>made me want to learn and make this site!</li></ol>
+            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://youtu.be/GWc7QppCPHM?si=gGIfwSa_TmGB_wuO" target="_blank" className=" hover:text-blue-300">Muck Video by Dani</a></div>
+            <ol className="list-disc list-inside text-gray-500"><li>videos like thse motivated me to continue game dev</li></ol>
+            <div className="flex space-x-2 place-items-center"><FaLink className="fill-gray-500 size-8"/><a href="https://freesound.org/" target="_blank" className=" hover:text-blue-300">freesound.org</a></div>
+            <div className="flex space-x-2 place-items-center"><FaLink className="fill-gray-500 size-8"/><a href="https://sonniss.com/gameaudiogdc/" target="_blank" className=" hover:text-blue-300">Sonniss GDC</a></div>
+            <ol className="list-disc list-inside text-gray-500"><li>free assets!</li></ol>
+            <div className="flex space-x-2 place-items-center"><FaYoutube className="fill-red-600 size-8"/><a href="https://youtu.be/Cd25H3TwYJE?si=UDmclGTJkXzLOZxp" target="_blank" className=" hover:text-blue-300">Home Safety Hotline</a></div>
         </div>
     </div>
 );
